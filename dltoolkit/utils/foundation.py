443c83adc0c5fddecf521a6a56779469f8c30d4e
Geoff Breemer <gmbreemer@gmail.com>
2018-02-11 14:56:29 +1000

    feat: Added cosine similarity, clip

--- dltoolkit/utils/foundation.py
+++ dltoolkit/utils/foundation.py
@@ -1,6 +1,7 @@
 """Basic functions"""
 import numpy as np
 
+
 def softmax(x):
     """Apply softmax to numpy vector x"""
     x_exp = np.exp(x-np.max(x))
@@ -10,3 +11,36 @@ def softmax(x):
 def sigmoid(x):
     """Apply the sigmoid function to numpy vector x"""
     return 1/(1+np.exp(-x))
+
+
+def cosine_similarity(u, v):
+    """
+    Calculate the cosine similarity between two numpy vectors
+    :param u: first vector
+    :param v: second vector
+    :return: cosine similarity
+    """
+    norm_u = np.sqrt(np.sum(np.square(u)))
+    norm_v = np.sqrt(np.sum(np.square(v)))
+
+    return np.dot(u, v) / (norm_u * norm_v)
+
+
+def clip(gradients, max_value):
+    """
+    Clip a dictionary of numpy arrays to a maximum value, clipping is done *in-place*
+    :param gradients: dictionary of numpy arrays
+    :param max_value: value to clip array elements to
+    """
+    for key, value in gradients.items():
+        np.clip(value, -max_value, max_value, out=gradients[key])
+
+
+if __name__ == '__main__':
+    grads = {"dx": np.array([1,2, 3]),
+             "dy": np.array([-1,-2, -3]),
+             "dz": np.array([3,4, -5])}
+
+
+    clip(grads, 2)
+    print(grads)
