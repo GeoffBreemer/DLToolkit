8c739c2afbac4078678c37e9d50cb1e934b3ffed
Geoff Breemer <gmbreemer@gmail.com>
2018-04-06 20:37:07 +1000

    feat: Added Ubuntu version of gb_setup_server.sh

--- scripts/gb_setup_server_MACOS.sh
+++ scripts/gb_setup_server_MACOS.sh
@@ -16,10 +16,8 @@ cd ssl
 certdir=$(pwd)
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout gb_cert.key -out gb_cert.pem -batch
 
-#cd ~
-#sed -i "" -e $"$ a\\
-
-sed -i "$ a\
+cd ~
+sed -i "" -e $"$ a\\
 c = get_config()\\
 c.NotebookApp.certfile = u'$certdir/gb_cert.pem'\\
 c.NotebookApp.keyfile = u'$certdir/gb_cert.key'\\
@@ -37,10 +35,10 @@ mkdir ~/dl/output
 mkdir ~/dl/output/segmentation_maps
 mkdir ~/dl/savedmodels
 
-echo Install Python packages, first activation may take some time
+echo Install Python packages (first activation may take some time)
 source activate tensorflow_p36
 pip install --upgrade pip
 pip install h5py sklearn progressbar2 pydot pydot_ng graphviz
 
-echo Update PYTHONPATH, ignore the error message
+echo Update PYTHONPATH (ignore the error message)
 sed -i "" -e $'$ a\\\n''export PYTHONPATH=/home/ubuntu/dl:$PYTHONPATH' ~/.bashrc
